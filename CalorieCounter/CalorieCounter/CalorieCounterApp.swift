4e373f2b705a3830fc21e7409a671fd213dd2bdc
marinmarkelic <marin.markelic.ets@gmail.com>
2022-09-08 11:28:05 +0200

    Created project

---
+++ CalorieCounter/CalorieCounter/CalorieCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCounterApp.swift
+//  CalorieCounter
+//
+//  Created by Marin on 08.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct CalorieCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
